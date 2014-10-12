45e383aca4de947c4857ef8d83ea6f45845fd6e3
Konrad <knrd.lisiecki@gmail.com>
2014-10-12 22:22:22 +0200

    OTWS: tab to spaces

--- subset.py
+++ subset.py
@@ -1,19 +1,20 @@
 import copy
 
+
 class Subset:
 
-	def __init__(self, n):
-		self.subsets = [[]]
-		for i in range(1, n+1):
-			new = []
-			for elt in self.subsets:
-				x = copy.deepcopy(elt)
-				x.append(i)
-				new.append(x) 
-			self.subsets.extend(new)
+    def __init__(self, n):
+        self.subsets = [[]]
+        for i in range(1, n+1):
+            new = []
+            for elt in self.subsets:
+                x = copy.deepcopy(elt)
+                x.append(i)
+                new.append(x)
+            self.subsets.extend(new)
 
-	def subsetSize(self, x):
-		return len(filter(lambda subset: len(subset) == x, self.subsets))
-			
+    def subsetSize(self, x):
+        return len(filter(lambda subset: len(subset) == x, self.subsets))
+
 if __name__ == "__main__":
     main()
