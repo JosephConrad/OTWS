f97698d438974fb201cbc5a6def451830ccfb7eb
Konrad <knrd.lisiecki@gmail.com>
2014-10-13 22:28:42 +0200

    OTWS: accelerate subset

--- subset.py
+++ subset.py
@@ -1,20 +1,24 @@
 import copy
+import sys
 
 
 class Subset:
 
     def __init__(self, n):
-        self.subsets = [[]]
+        self.subsets = [""]
         for i in range(1, n+1):
             new = []
             for elt in self.subsets:
-                x = copy.deepcopy(elt)
-                x.append(i)
+                x = elt 
+                x += str(i)
                 new.append(x)
-            self.subsets.extend(new)
+            self.subsets += new
 
     def subsetSize(self, x):
         return len(filter(lambda subset: len(subset) == x, self.subsets))
+
+def main():
+	subset = Subset(int(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
